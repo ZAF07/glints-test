be887252766f369c94fef207f7088fe252392497
ZAF07 <zaffere07@gmail.com>
2021-10-09 13:20:19 +0800

    added config file for DB

---
+++ model/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    username: 'zaffere',
+    password: null,
+    database: 'glints',
+    host: '127.0.0.1',
+    port: '5432',
+    dialect: 'postgres',
+  },
+};
