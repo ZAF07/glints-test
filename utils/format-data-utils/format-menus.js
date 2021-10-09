87ab5886c16685aa4e99ec3b5c58e12cfb3807cc
ZAF07 <zaffere07@gmail.com>
2021-10-09 22:50:06 +0800

    removed commented out code

--- utils/format-data-utils/format-menus.js
+++ utils/format-data-utils/format-menus.js
@@ -1,6 +1,5 @@
 const getDishes = (arr) => {
   const restaurants = arr;
-  // console.log('ress => ', restaurants);
   const menuForEachRest = [];
   restaurants.forEach(rest => {
     const restMenu = {
@@ -8,16 +7,13 @@ const getDishes = (arr) => {
       menu: []
     };
     const dishesAndPrices = rest.menu;
-    // console.log('dsa => ', dishesAndPrices);
     //  ABSTRACT EACH DISH
     dishesAndPrices.forEach(dish => {
       restMenu.menu.push(dish);
 
     })
     menuForEachRest.push(restMenu);
-    // console.log('dish ==---> ', restMenu);
   });
   return menuForEachRest;
-  // console.log('ALL RESTARUANT ==> ', menuForEachRest[100]);
 }
  exports.getDishes = getDishes;
