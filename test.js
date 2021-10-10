3c96ca26cc59a14de981d529e9686f096e3351f8
ZAF07 <zaffere07@gmail.com>
2021-10-10 17:49:44 +0800

    refactored test file for better readibility

--- test.js
+++ test.js
@@ -1,68 +1,100 @@
 const chai = require('chai');
 const describe = require('mocha').describe;
+const getRawData = require('./utils/general-utils/get-file.js')
+const getData = getRawData.getData;
+
+const file = './datafile/restaurant_with_menu.json';
+const userfile = './datafile/users_with_purchase_history.json';
 
 const expect = chai.expect;
 
+describe('GETS FILE FROM GIVEN JSON FILE AND TRANSFORMS IT INTO SEED READY FORMAT WITH NECESSARY DETAILS -> ', () => {
 
-const getRawData = require('./utils/general-utils/get-file.js')
-const file = './datafile/restaurant_with_menu.json';
-const userfile = './datafile/users_with_purchase_history.json';
-const getData = getRawData.getData;
-// console.log(getData.getData());
-//  MOCK SEADER FILE
-const seederFileEnv = async (typeOfFile) => {
-  let fileToTest;
-  const timeFile = await getData(file, 'time');
-  const menuFile = await getData(file, 'menu');
-  const purchaseFile = await getData(userfile, 'purchases');
-  // console.log('FILE -> ', timeFile[0]);
+  describe('RESTAURANT DETAILS', () => {
+    it('Each RESTAURANT object has required properties to seed data', async () => {
+      
+      const results = await getData(file,'restaurant');
   
-  if (typeOfFile == 'users' || 'purchases') {
-    return fileToTest = await getData(userfile, `${typeOfFile}` )
-  }
-  fileToTest =  await getData(file, 'time')
+      for (let i = 0; i < results.length; i += 1) {
+        // console.log(results[i]);
+        expect(results[i]).to.have.keys('name', 'cash_balance', 'created_at', 'updated_at')
+      }
+    })
+  });
 
-  // STILL FIGURING THIS OUT 👇
-
-  // switch (typeOfFile) {
-  //   case 'users':
-  //     return await getData(userfile, `${typeOfFile}`)
-  //     break;
-  //   case 'time':
-  //     return await getData(file, `${typeOfFile}`)
-  //   default:
-  //     break;
-  // }
-
+  // USER SEED FILE
+  describe('USER DETAILS', () => {
+    it('Each User object has required properties to seed data', async () => {
+      
+      const results = await getData(userfile,'users');
   
-return fileToTest  
-};
-// seederFileEnv();
+      for (let i = 0; i < results.length; i += 1) {
+        // console.log(results[i]);
+        expect(results[i]).to.have.keys('name', 'cash_balance', 'created_at', 'updated_at')
+      }
+    })
+  });
 
-describe('Gets file from given JSON file and transforms it into seed ready format with necessary USER information', () => {
-  it('Each User object has required properties to seed data', async () => {
-    const results = await seederFileEnv('users');
+  // PURCHASES SEED FILE
+  describe('PURCHASES DETAILS', () => {
+    it('Each Purchase object has required properties to seed data', async () => {
+       // purchases list for all users 
+      const results = await getData(userfile,'purchases');
+  
+      for (let i = 0; i < results.length; i += 1) {
+        // purchase list for one user
+        const purchasesList = results[i];
+  
+        for (let j = 0; j < purchasesList.length; j += 1) {
+  
+          expect(purchasesList[j]).to.have.keys('dish', 'restaurant_name', 'transaction_amt', 'transaction_date', 'created_at', 'updated_at')
+        }
+      }
+    })
+  });
 
-    for (let i = 0; i < results.length; i += 1) {
-      // console.log(results[i]);
-      expect(results[i]).to.have.keys('name', 'cash_balance', 'created_at', 'updated_at')
-    }
-  })
-})
 
-// describe('Gets file from given JSON file and transforms it into seed ready format with necessary  OPENING HOURS information', () => {
-//   it('Each Opening Time object has required properties to seed data', async () => {
-//     const results = await seederFileEnv('time');
+  // OPENING HOURS SEED
+  describe('OPENING HOURS DETAILS', () => {
+    it('Each Opening Time object has required properties to seed data', async () => {
+      const results = await getData(file, 'time');
+  
+      // represents single restaurant ( has up to 7 opening days)
+      for (let i = 0; i < results.length; i += 1) {
+        const currentRestaurantTiming = results[i];
+  
+        // represents individual days
+        for (let j = 0; j < currentRestaurantTiming.length; j += 1) {
+          expect(currentRestaurantTiming[j]).to.have.keys('day', 'open_hours', 'opening_time', 'closing_time', 'created_at', 'updated_at')
+  
+        }
+      }
+    })
+  });
 
-//     // represents single restaurant ( has up to 7 opening days)
-//     for (let i = 0; i < results.length; i += 1) {
-//       const currentRestaurantTiming = results[i];
 
-//       // represents individual days
-//       for (let j = 0; j < currentRestaurantTiming.length; j += 1) {
-//         expect(currentRestaurantTiming[j]).to.have.keys('day', 'open_hours', 'opening_time', 'closing_time', 'created_at', 'updated_at')
+  // MENU SEED FILE
+  describe('MENU DETAILS', () => {
+    it('Each Menu object has required properties to seed data', async () => {
+      const results = await getData(file, 'menu');
+  
+      // represents single restaurant ( has up to 7 opening days)
+      for (let i = 0; i < results.length; i += 1) {
+        const currentRestaurantTiming = results[i];
+  
+        // represents individual days
+        for (let j = 0; j < currentRestaurantTiming.length; j += 1) {
+          expect(currentRestaurantTiming[j]).to.have.keys('dish', 'price', 'created_at', 'updated_at')
+  
+        }
+      }
+    })
+  });
 
-//       }
-//     }
-//   })
-// })
+});
+
+
+
+
+
+
