492b0aa74490c91c733475ba13fc45661b8dade0
ZAF07 <zaffere07@gmail.com>
2021-10-11 10:30:01 +0800

    added api key mock

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.json({message: 'Hello welcome!', APIKEY: 'apiKey'});
 });
  
 app.use('/api', restaurantOpeningTime);
